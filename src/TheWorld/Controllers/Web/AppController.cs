2383550a36c5493a1d12aa7e8d44b91a5f9de2a6
Bobby Montalvo <bmontalvo@movietickets.com>
2017-01-10 18:11:58 -0500

    Finished 'Using Tag Helpers' submodule of the 'MVC 6' module.

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
