2ac7b04fba8044cba1f1432e5796c66d09c4d13e
Bobby Montalvo <bmontalvo@movietickets.com>
2017-01-10 17:29:12 -0500

    Finished JavaScript module.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿// site.js
+(function () {
+
+    //var ele = $("#username");
+    //ele.text("Bobby Montalvo");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.css("background", "#888");
+    //});
+    //main.on("mouseleave", function () {
+    //    main.css("background", "");
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
